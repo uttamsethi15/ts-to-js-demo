293e1015ea275b82c69c45ea0f36f377075865ef
uttamsethi15 <uttam.sethi@hcl-software.com>
2026-02-24 20:21:28 +0530

    Auto-generated JS from TypeScript

--- dist/lambda_function.js
+++ dist/lambda_function.js
@@ -5,7 +5,7 @@ const handler = async (event) => {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: "Hello from TypeScript Lambda"
+            message: "Hello from TypeScript github"
         })
     };
 };
